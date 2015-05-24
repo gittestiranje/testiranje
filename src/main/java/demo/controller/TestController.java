6dfee14d3596da75b5f25b66985940047fc52a20
Git Testiranje <git.testiranje@gmail.com>
2015-05-24 16:58:48 +0200

    Add support for Spring-Loaded

---
+++ src/main/java/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+	@RequestMapping(value = "/message", method = RequestMethod.GET)
+	@ResponseBody
+	public String getMessage() {
+		return "test";
+	}
+
+}
