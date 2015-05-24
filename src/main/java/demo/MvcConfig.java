e9aefe2a4407bfdf27c86f7e81ead4584f4cac9e
Git Testiranje <git.testiranje@gmail.com>
2015-05-24 16:11:16 +0200

    Use yaml
    Some test yml files are created

--- src/main/java/demo/MvcConfig.java
+++ src/main/java/demo/MvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@Profile("production")
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
