6cafe21d3f79440e813492eab9d7aacc942a0c73
Git Testiranje <git.testiranje@gmail.com>
2015-05-24 16:22:00 +0200

    Fix #2 - homepage not displayed

--- src/main/java/demo/MvcConfig.java
+++ src/main/java/demo/MvcConfig.java
@@ -1,11 +1,12 @@
 package demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@Profile("production")
+//@Profile("production")
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
